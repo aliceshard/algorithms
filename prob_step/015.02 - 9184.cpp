6ec205409b3a4f9fe312f86fcc6628567ed068ef
pmang kim <aliceshard42@gmail.com>
2021-08-01 22:35:53 +0900

    8/1 optimize 9184

--- prob_step/015.02 - 9184.cpp
+++ prob_step/015.02 - 9184.cpp
@@ -7,8 +7,7 @@
 #include <cstring>
 using namespace std;
 
-int memo[21][21][21] = { 0, };
-bool memo_ready[21][21][21] = { false, };
+int memo[21][21][21];
 
 int w(int a, int b, int c) {
 	if (a <= 0 || b <= 0 || c <= 0) {
@@ -17,7 +16,7 @@ int w(int a, int b, int c) {
 	if (a > 20 || b > 20 || c > 20) {
 		return w(20, 20, 20);
 	}
-	if (memo_ready[a][b][c] == true) {
+	if (memo[a][b][c] != -1) {
 		return memo[a][b][c];
 	}
 	if (a < b && b < c) {
@@ -25,14 +24,7 @@ int w(int a, int b, int c) {
 		int w2 = w(a, b - 1, c - 1);
 		int w3 = w(a, b - 1, c);
 
-		memo[a][b][c - 1] = w1;
-		memo[a][b - 1][c - 1] = w2;
-		memo[a][b - 1][c] = w3;
-		memo_ready[a][b][c - 1] = true;
-		memo_ready[a][b - 1][c - 1] = true;
-		memo_ready[a][b - 1][c] = true;
-
-		return w1 + w2 - w3;
+		return memo[a][b][c] = w1 + w2 - w3;
 	}
 
 	int w1 = w(a - 1, b, c);
@@ -40,24 +32,16 @@ int w(int a, int b, int c) {
 	int w3 = w(a - 1, b, c - 1);
 	int w4 = w(a - 1, b - 1, c - 1);
 
-	memo[a - 1][b][c] = w1;
-	memo[a - 1][b - 1][c] = w2;
-	memo[a - 1][b][c - 1] = w3;
-	memo[a - 1][b - 1][c - 1] = w4;
-	memo_ready[a - 1][b][c] = true;
-	memo_ready[a - 1][b - 1][c] = true;
-	memo_ready[a - 1][b][c - 1] = true;
-	memo_ready[a - 1][b - 1][c - 1] = true;
-
-	return w1 + w2 + w3 - w4;
+	return memo[a][b][c] = w1 + w2 + w3 - w4;
 }
 
 int main(void) {
 	int a, b, c;
-	while(true) {
-		scanf("%d %d %d", &a, &b, &c);
-		if (a == -1 && b == -1 && c == -1)
-			break;
+	memset(memo, -1, sizeof(memo));
+
+	scanf("%d %d %d", &a, &b, &c);
+	while(!(a == -1 && b == -1 && c == -1)) {
 		printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+		scanf("%d %d %d", &a, &b, &c);
 	}
 }
