4b03ee32c2820ec2fb37f57477f90453f3c6d228
pmang kim <aliceshard42@gmail.com>
2021-08-19 00:49:28 +0900

    8/19

--- baekjoon_prob_step/018.05 - 1874.py
+++ baekjoon_prob_step/018.05 - 1874.py
@@ -1,30 +1,37 @@
-n=int(input())
-st = [x for x in range(n+1,0,-1)]
+n = int(input())
+st = [x for x in range(n + 1, 0, -1)]
 workst = []
-arr=[]
-for i in range(0,n):
+result = []
+arr = []
+impossible = False
+for i in range(0, n):
 	arr.append(int(input()))
 
-#init
+# init
 workst.append(0)
 for i in range(0, arr[0]):
-	print('+')
+	result.append('+')
 	workst.append(st[-1])
 	st.pop()
-	
-#main logic
-for i in range(0,n):
+
+# main logic
+for i in range(0, n):
 	if arr[i] < workst[-1]:
 		while arr[i] != workst[-1]:
-			print('-')
+			result.append('-')
 			workst.pop()
 	elif arr[i] > workst[-1]:
 		if st[-1] > arr[i]:
-			print('impossible')
+			print('NO')
+			impossible = True
 			break
 		while arr[i] != workst[-1]:
-			print('+')
+			result.append('+')
 			workst.append(st[-1])
 			st.pop()
-	print('-')
+	result.append('-')
 	workst.pop()
+
+if impossible == False:
+	for i in result:
+		print(i)
