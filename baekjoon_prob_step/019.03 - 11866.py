f68ec86c0b5b5bcaaf17be4213f79ce4aed6ae87
aliceshard <aliceshard42@gmail.com>
2021-08-23 22:44:12 +0900

    mb

--- baekjoon_prob_step/019.03 - 11866.py
+++ baekjoon_prob_step/019.03 - 11866.py
@@ -4,7 +4,14 @@ n,k=list(map(int,input().split()))
 arr=[x for x in range(1,n+1)]
 qu=deque([])
 
-cnt=0
+cnt=-1
 for i in range(0,n):
-	cnt = (cnt + (k-1)) % (n-i)
-	arr.
+	print(arr)
+	cnt = (cnt + k) % n
+	if(arr[cnt]==-1):
+		while arr[cnt]!=-1:
+			cnt = (cnt+1)%n
+			print(cnt)
+	qu.append(arr[cnt])
+	arr[cnt]=-1
+print(qu)
