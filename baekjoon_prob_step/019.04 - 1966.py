81dc2d701ade111416f5e078ec48de370218aedc
pmang kim <aliceshard42@gmail.com>
2021-08-30 01:12:46 +0900

    8/30

--- baekjoon_prob_step/019.04 - 1966.py
+++ baekjoon_prob_step/019.04 - 1966.py
@@ -1,17 +1,22 @@
 from collections import deque
 
 c = int(input())
-for i in range(0,c):
-	n, m = list(map(int,input().split()))
-	arr =list(map(int,input().split())) 
-	max_pr=max(arr)
-	q=deque(arr)
-	
-	pr_cnt=1
-	idx_cnt=0
-	while q.empty() != True:
-		if q[0] < max_pr:
-			q.append(q.popleft())
-			idx_cnt += 1
-			continue
-		
+for i in range(0, c):
+    n, m = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
+    arr2 = [(i, arr[i]) for i in range(0,n)]
+    max_pr = max(arr)
+    q2 = deque(arr2)
+
+    pr_cnt = 0
+    while len(q2) != 0:
+        if q2[0][1] < max_pr:
+            q2.append(q2.popleft())
+            continue
+
+        pr_cnt += 1
+        if q2[0][0] == m:
+            print(pr_cnt)
+            break
+        q2.popleft()
+        max_pr = max(list(q2), key=lambda x:x[1])[1]
