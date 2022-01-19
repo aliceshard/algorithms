89b2764036365c99ff8985e36edcfab431d47680
aliceshard <aliceshard42@gmail.com>
2022-01-19 14:26:16 +0900

    1/18

--- baekjoon_prob_step/020.07 - 10830.py
+++ baekjoon_prob_step/020.07 - 10830.py
@@ -1,12 +1,14 @@
-def vec_mul(vec, n):
-	print('calculating...')
+def vec_mul(vec1, vec2, n):
+	#print('calculating...')
 	result = []
+	md = 1000
 	for i in range(0, n):
 		temp = []
 		for j in range(0, n):
 			acc = 0
 			for k in range(0, n):
-				acc += vec[i][k] * vec[k][j]
+				acc += (vec1[i][k] * vec2[k][j])%md
+			acc %= md
 			temp.append(acc)
 		result.append(temp)
 	return result
@@ -15,16 +17,50 @@ def factor_bin(b):
 	temp = []
 	while(b != 0):
 		if b % 2 == 0:
-			b 
+			temp.append(0)
+			b //= 2
+		elif b % 2 == 1:
+			temp.append(1)
+			b //= 2
+	#print('factored bin: {}'.format(temp))
+	return temp
 
+def init_idv(n):
+	idv = []
+	for i in range(0,n):
+		temp = []
+		for j in range(0,n):
+			if i == j:
+				temp.append(1)
+			else:
+				temp.append(0)
+		idv.append(temp)
+	return idv
+
+def vec_proc(vec, n, b):
+	bin_str = factor_bin(b)
+	acc_vec = vec
+	res_vec = init_idv(n)
+	for i in range(0, len(bin_str)):
+		if bin_str[i] == 1:
+			#print('bit is one...')
+			res_vec = vec_mul(res_vec, acc_vec,n)
+		acc_vec = vec_mul(acc_vec, acc_vec, n)
+		#print(acc_vec)
+	return res_vec
+	
 #print('getting vector input...')
-size = list(map(int, input().split()))
+n, b = list(map(int, input().split()))
 vec=[]
-for i in range(0, size2[0]):
+result = []
+for i in range(0, n):
 	temp = list(map(int, input().split()))
 	vec.append(temp)
 
-for i in range(0, size1[0]):
-	for j in range(0, size2[1]):
+result = vec_proc(vec, n, b)
+
+# print
+for i in range(0, n):
+	for j in range(0, n):
 		print('{}'.format(result[i][j]), end=' ')
 	print('')
