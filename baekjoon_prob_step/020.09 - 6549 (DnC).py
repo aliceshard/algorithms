02eb089ad489753b71689aea295d13cdd50ac980
pmang kim <aliceshard42@gmail.com>
2022-01-28 13:26:33 +0900

    1/28

--- baekjoon_prob_step/020.09 - 6549 (DnC).py
+++ baekjoon_prob_step/020.09 - 6549 (DnC).py
@@ -1,58 +1,67 @@
 # parameters are index of input array
 def solve(start, end):
     global input_arr
-    mid = (start + end) // 2
-
-    # max heights of the left partition and the right partition
-    left_area = 0
-    right_area = 0
-    mid_area = 0
-
+    left_idx = start
+    mid_idx = (start + end) // 2
+    right_idx = end
+    #print('left_idx: {}, right_idx: {}'.format(left_idx, right_idx))
     if start == end:
+        #print('returned: {}'.format(input_arr[start]))
         return input_arr[start]
-    else:
-        # calculating max area of left partition
-        width = 0
-        height_max = input_arr[start]
-        for i in range(0, end+1):
-            if input_arr[i] >= height_max:
-                width += 1
-            elif input_arr[i] < height_max:
-                left_area = height_max * width
-                break
+    area = max(solve(start, mid_idx), solve(mid_idx + 1, end)) # to be determined
 
-        # calculating max area of right partition
-        width = 0
-        height_max = input_arr[end]
-        for i in range(end, -1, -1):
-            if input_arr[i] >= height_max:
-                width += 1
-            elif input_arr[i] < height_max:
-                right_area = height_max * width
-                break
+    #if left_idx == 0 and right_idx == len(input_arr)-1:
+    #    print('this is root case')
 
-        # calculating local mid here
-        height_max = input_arr[mid]
-        left_idx = right_idx = mid
-        while True:
-            if input_arr[left_idx] >= height_max:
-                height_max = input_arr[left_idx]
+    left_idx = right_idx = mid_idx
+    minH = input_arr[mid_idx]
+    lborder_flag = False
+    rborder_flag = False
+
+    # There's only one idx displacement per loop
+    while lborder_flag == False or rborder_flag == False:
+        if left_idx == start:
+            lborder_flag = True
+        if right_idx == end:
+            rborder_flag = True
+
+        # case 1 : if mid_idx is surrounded with higher heights -> proceed both idx's
+        if lborder_flag == False:
+            if input_arr[left_idx - 1] >= input_arr[left_idx]:
                 left_idx -= 1
-            elif input_arr[left_idx] < height_max:
-                break
-        height_max = input_arr[mid]
-        while True:
-            if input_arr[right_idx] >= height_max:
-                height_max = input_arr[right_idx]
+                area = max(area, minH * (right_idx - left_idx + 1))
+                continue
+        elif rborder_flag == False:
+            if input_arr[right_idx + 1] >= input_arr[right_idx]:
                 right_idx += 1
-            elif input_arr[right_idx] < height_max:
-                break
-        if left_idx == right_idx:
-            mid_area = input_arr[left_idx]
-        else:
-            width = right_idx - left_idx + 1
-            mid_area = width * height_max
-        return max([left_area, mid_area, right_area])
+                area = max(area, minH * (right_idx - left_idx + 1))
+                continue
+
+        # case 2 : if mid_idx is surrounded with lower heights
+        # -> compare left_idx and right_idx and proceed the higher idx
+        if lborder_flag == True and rborder_flag == False:
+            right_idx += 1
+            minH = min(minH, input_arr[right_idx])
+            area = max(area, (right_idx - left_idx + 1) * minH)
+            continue
+        elif lborder_flag == False and rborder_flag == True:
+            left_idx -= 1
+            minH = min(minH, input_arr[left_idx])
+            area = max(area, (right_idx - left_idx + 1) * minH)
+            continue
+        elif lborder_flag == False and rborder_flag == False:
+            if input_arr[left_idx - 1] <= input_arr[right_idx + 1]:
+                right_idx += 1
+                minH = min(minH, input_arr[right_idx])
+                area = max(area, (right_idx - left_idx + 1) * minH)
+                continue
+            elif input_arr[left_idx - 1] >= input_arr[right_idx + 1]:
+                left_idx -= 1
+                minH = min(minH, input_arr[left_idx])
+                area = max(area, (right_idx - left_idx + 1) * minH)
+                continue
+    #print('returned: {}'.format(area))
+    return area
 
 input_arr = []
 while True:
@@ -60,3 +69,5 @@ while True:
     input_arr = arr[1:]
     if arr[0] == 0:
         break
+    ans = solve(0, len(input_arr)-1)
+    print(ans)
